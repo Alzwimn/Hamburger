1a97a9986db151702b0150aa523ac3f520d840b0
Alzwimn <53050916+Alzwimn@users.noreply.github.com>
2020-03-08 17:01:02 -0300

    Add files via upload

--- hamburger.js
+++ hamburger.js
@@ -1,13 +1,36 @@
 let hamburger = document.querySelector(".s-hamburger");
+let clicked = false;
 const hamburgerToggle = () => {
     hamburger.addEventListener("click", () => {
         let innerCircle = document.querySelector(".h-inner-circle");
         let lineH = document.querySelector(".h-lineH");
         let lineV = document.querySelector(".h-lineV");
+        
+        if(!clicked){
+            clicked = true;
+            hamburger.classList.add("s-hamburger-active");
+            hamburger.classList.remove("s-hamburger-active-reverse");
 
-        hamburger.classList.toggle("s-hamburger-active");
-        lineH.classList.toggle("s-line-active")
-        lineV.classList.toggle("s-line-active");
+            lineH.classList.add("s-line-active");
+            lineH.classList.remove("s-line-active-reverse");
+
+            lineV.classList.add("s-line-active");
+            lineV.classList.remove("s-line-active-reverse");
+
+        }else{
+            clicked = false;
+            hamburger.classList.add("s-hamburger-active-reverse");
+            hamburger.classList.remove("s-hamburger-active");
+
+            lineH.classList.add("s-line-active-reverse");
+            lineH.classList.remove("s-line-active");
+
+            lineV.classList.add("s-line-active-reverse");
+            lineV.classList.remove("s-line-active");
+        }
+        //hamburger.classList.toggle("s-hamburger-active");
+        //lineH.classList.toggle("s-line-active")
+        //lineV.classList.toggle("s-line-active");
     });
 }
 hamburgerToggle();
